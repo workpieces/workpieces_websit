56e38a78b291fa997d497ab29a24f539f32c2b5a
taoshumin_vendor <taoshumin@foxmail.com>
2022-05-18 20:54:58 +0800

    feat: 添加logo

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -1,2 +1,7 @@
 // GITHUB Workpieces官方地址
 const GITHUB = "https://github.com/workpieces";
+const APPLESTORE = "https://apps.apple.com/cn/app/etcdwp/id1617626187?mt=12";
+const DMG = "https://github.com/workpieces/etcdWp/releases";
+const ETCDNAME = "etcdWp";
+const ETCDDESC =
+    "etcdWp是一款免费且支持Macos桌面的etcd客户端桌面。支持对etcd单机版和集群版键值存储做增删改查、集群资源查看、租约用户角色管理等操作。";
--- lib/page/HomePage.dart
+++ lib/page/HomePage.dart
@@ -1,51 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_workpieces/constant/constant.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatelessWidget {
-  List<Widget> pageChildren(double width) {
+  List<Widget> pageChildren() {
     return <Widget>[
-      Container(
-        width: width,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              "Website \nDevelopers",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 40.0,
-                  color: Colors.white),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 20.0),
-              child: Text(
-                "We have taken each and every project handed over to us as a challenge, which has helped us achieve a high project success rate.",
-                style: TextStyle(fontSize: 16.0, color: Colors.white),
+      Expanded(
+        flex: 2,
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                ETCDNAME,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 40.0,
+                    color: Colors.white),
               ),
-            ),
-            MaterialButton(
-              color: Colors.white,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
-              onPressed: () {},
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 20.0, horizontal: 40.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
                 child: Text(
-                  "Our Packages",
-                  style: TextStyle(color: Colors.red),
+                  ETCDDESC,
+                  style: TextStyle(fontSize: 16.0, color: Colors.white),
                 ),
               ),
-            )
-          ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  MaterialButton(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                    onPressed: () {
+                      launch(APPLESTORE);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20.0, horizontal: 40.0),
+                      child: Text(
+                        "Apple Store 下载",
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 44.0,
+                  ),
+                  MaterialButton(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                    onPressed: () {
+                      launch(DMG);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 20.0, horizontal: 40.0),
+                      child: Text(
+                        "DMG 下载",
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
-      Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20.0),
-        child: Image.asset(
-          "assets/images/lp_image.png",
-          width: width,
-        ),
-      )
+      Expanded(
+          flex: 2,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            child: Image.asset(
+              "assets/images/etcd_image.png",
+              fit: BoxFit.fitHeight,
+              height: 350,
+            ),
+          ))
     ];
   }
 
@@ -55,7 +94,7 @@ class HomePage extends StatelessWidget {
       builder: (context, constraints) {
         return Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: pageChildren(constraints.biggest.width / 2),
+          children: pageChildren(),
         );
       },
     );
